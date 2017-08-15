d371862118cc925629b0d05e15cfb4db3d684da3
Asun <asun0204@163.com>
2017-08-15 12:49:26 +0800

    jupyter botebook

--- second.py
+++ second.py
@@ -4,3 +4,191 @@
 # Created by iFantastic on 2017/8/14
 # Description : http://www.cnblogs.com/cutd/p/5713525.html
 
+import pandas as pd
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.linear_model import LogisticRegression
+import re
+import operator
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# A function to get the title from a name.
+def get_title(name):
+    # Use a regular expression to search for a title.  Titles always consist of capital and lowercase letters, and end with a period.
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+    # If the title exists, extract and return it.
+    if title_search:
+        return title_search.group(1)
+    return ""
+
+# A dictionary mapping family name to id
+family_id_mapping = {}
+
+
+# A function to get the id given a row
+def get_family_id(row):
+    # Find the last name by splitting on a comma
+    last_name = row["Name"].split(",")[0]
+    # Create the family id
+    family_id = "{0}{1}".format(last_name, row["FamilySize"])
+    # Look up the id in the mapping
+    if family_id not in family_id_mapping:
+        if len(family_id_mapping) == 0:
+            current_id = 1
+        else:
+            # Get the maximum id from the mapping and add one to it if we don't have an id
+            current_id = (max(family_id_mapping.items(), key=operator.itemgetter(1))[1] + 1)
+        family_id_mapping[family_id] = current_id
+    return family_id_mapping[family_id]
+
+# 读取数据
+titanic = pd.read_csv('Data/train.csv')
+titanic_test = pd.read_csv('Data/test.csv')
+
+# 数据清洗
+# 处理Age中的缺失值
+titanic['Age'] = titanic['Age'].fillna(titanic['Age'].median())
+# 量化Sex
+titanic.loc[titanic['Sex'] == 'male', 'Sex'] = 0
+titanic.loc[titanic['Sex'] == 'female', 'Sex'] = 1
+# 量化Embarked
+titanic['Embarked'] = titanic['Embarked'].fillna('S')
+titanic.loc[titanic['Embarked'] == 'S', 'Embarked'] = 0
+titanic.loc[titanic['Embarked'] == 'C', 'Embarked'] = 1
+titanic.loc[titanic['Embarked'] == 'Q', 'Embarked'] = 2
+
+
+# 生成新特征
+# Generating a familysize column
+titanic["FamilySize"] = titanic["SibSp"] + titanic["Parch"]
+# The .apply method generates a new series
+titanic["NameLength"] = titanic["Name"].apply(lambda x: len(x))
+# Get all the titles and print how often each one occurs.
+titles = titanic["Name"].apply(get_title)
+
+# Map each title to an integer.  Some titles are very rare, and are compressed into the same codes as other titles.
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 6, "Major": 7, "Col": 7, "Mlle": 8, "Mme": 8, "Don": 9, "Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9, "Capt": 7, "Ms": 2}
+for k, v in title_mapping.items():
+    titles[titles == k] = v
+# Add in the title column.
+titanic["Title"] = titles
+
+# Get the family ids with the apply method
+family_ids = titanic.apply(get_family_id, axis=1)
+# There are a lot of family ids, so we'll compress all of the families under 3 members into one code.
+family_ids[titanic["FamilySize"] < 3] = -1
+titanic["FamilyId"] = family_ids
+
+# 找到最好的特征
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]
+# Perform feature selection
+selector = SelectKBest(f_classif, k=5)
+selector.fit(titanic[predictors], titanic["Survived"])
+# Get the raw p-values for each feature, and transform from p-values into scores
+scores = -np.log10(selector.pvalues_)
+# Plot the scores.  See how "Pclass", "Sex", "Title", and "Fare" are the best?
+plt.bar(range(len(predictors)), scores)
+plt.xticks(range(len(predictors)), predictors, rotation='vertical')
+plt.show()
+# Pick only the four best features.
+predictors = ["Pclass", "Sex", "Fare", "Title"]
+alg = RandomForestClassifier(random_state=1, n_estimators=150, min_samples_split=8, min_samples_leaf=4)
+# Compute the accuracy score for all the cross validation folds.  (much simpler than what we did before!)
+scores = cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=3)
+# Take the mean of the scores (because we have one for each fold)
+
+# 集成
+# The algorithms we want to ensemble.
+# We're using the more linear predictors for the logistic regression, and everything with the gradient boosting classifier.
+algorithms = [
+    [GradientBoostingClassifier(random_state=1, n_estimators=25, max_depth=3), ["Pclass", "Sex", "Age", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]],
+    [LogisticRegression(random_state=1), ["Pclass", "Sex", "Fare", "FamilySize", "Title", "Age", "Embarked"]]
+]
+
+# Initialize the cross validation folds
+folder = KFold(n_splits=3, random_state=1)
+
+predictions = []
+for train, test in folder.split(titanic['Age']):
+    train_target = titanic["Survived"].iloc[train]
+    full_test_predictions = []
+    # Make predictions for each algorithm on each fold
+    for alg, predictors in algorithms:
+        # Fit the algorithm on the training data.
+        alg.fit(titanic[predictors].iloc[train,:], train_target)
+        # Select and predict on the test fold.
+        # The .astype(float) is necessary to convert the dataframe to all floats and avoid an sklearn error.
+        test_predictions = alg.predict_proba(titanic[predictors].iloc[test,:].astype(float))[:,1]
+        full_test_predictions.append(test_predictions)
+    # Use a simple ensembling scheme -- just average the predictions to get the final classification.
+    test_predictions = (full_test_predictions[0] + full_test_predictions[1]) / 2
+    # Any value over .5 is assumed to be a 1 prediction, and below .5 is a 0 prediction.
+    test_predictions[test_predictions <= .5] = 0
+    test_predictions[test_predictions > .5] = 1
+    predictions.append(test_predictions)
+
+# Put all the predictions together into one array.
+predictions = np.concatenate(predictions, axis=0)
+
+# Compute accuracy by comparing to the training data.
+accuracy = sum(predictions[predictions == titanic["Survived"]]) / len(predictions)
+
+# 在测试集上匹配我们的变化
+titanic_test = pd.read_csv('Data/test.csv')
+
+# 处理测试集
+titanic_test = pd.read_csv("Data/test.csv")
+titanic_test['Age'] = titanic_test['Age'].fillna(titanic_test['Age'].median())
+titanic_test['Fare'] = titanic_test['Fare'].fillna(titanic_test['Fare'].median())
+titanic_test.loc[titanic_test['Sex'] == 'male', 'Sex'] = 0
+titanic_test.loc[titanic_test['Sex'] == 'female', 'Sex'] = 1
+titanic_test['Embarked'] = titanic_test['Embarked'].fillna('S')
+titanic_test.loc[titanic_test['Embarked'] == 'S', 'Embarked'] = 0
+titanic_test.loc[titanic_test['Embarked'] == 'C', 'Embarked'] = 1
+titanic_test.loc[titanic_test['Embarked'] == 'Q', 'Embarked'] = 2
+
+# First, we'll add titles to the test set.
+titles = titanic_test["Name"].apply(get_title)
+# We're adding the Dona title to the mapping, because it's in the test set, but not the training set
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 6, "Major": 7, "Col": 7, "Mlle": 8, "Mme": 8, "Don": 9, "Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9, "Capt": 7, "Ms": 2, "Dona": 10}
+for k, v in title_mapping.items():
+    titles[titles == k] = v
+titanic_test["Title"] = titles
+
+# Now, we add the family size column.
+titanic_test["FamilySize"] = titanic_test["SibSp"] + titanic_test["Parch"]
+
+family_ids = titanic_test.apply(get_family_id, axis=1)
+family_ids[titanic_test["FamilySize"] < 3] = -1
+titanic_test["FamilyId"] = family_ids
+titanic_test["NameLength"] = titanic_test["Name"].apply(lambda x: len(x))
+
+# 在测试集上做预测
+predictors = ["Pclass", "Sex", "Age", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]
+
+algorithms = [
+    [GradientBoostingClassifier(random_state=1, n_estimators=25, max_depth=3), predictors],
+    [LogisticRegression(random_state=1), ["Pclass", "Sex", "Fare", "FamilySize", "Title", "Age", "Embarked"]]
+]
+
+full_predictions = []
+for alg, predictors in algorithms:
+    # Fit the algorithm using the full training data.
+    alg.fit(titanic[predictors], titanic["Survived"])
+    # Predict using the test dataset.  We have to convert all the columns to floats to avoid an error.
+    predictions = alg.predict_proba(titanic_test[predictors].astype(float))[:,1]
+    full_predictions.append(predictions)
+
+# The gradient boosting classifier generates better predictions, so we weight it higher.
+predictions = (full_predictions[0] * 3 + full_predictions[1]) / 4
+predictions[predictions <= .5] = 0
+predictions[predictions > .5] = 1
+predictions = predictions.astype(int)
+submission = pd.DataFrame({
+        "PassengerId": titanic_test["PassengerId"],
+        "Survived": predictions
+    })
+submission.to_csv('Data/submission.csv', index=False)
